fd8d5495047da9b102f1a5b86ea08ad66aae0595
Al Stone <astone@heckgrammar.co.uk>
2024-09-10 09:37:54 +0100

    Add project files.

---
+++ MoreSubroutines/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreSubroutines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 32 and 82");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            if (InRange(userInput))
+            {
+                Console.WriteLine("The number is in range");
+            }
+            else
+            {
+                Console.WriteLine("The number is not in range");
+            }
+        }
+
+        static bool InRange(int input)
+        {
+            if (input>=32 && input <= 82)
+            {
+                return true;
+            }
+            return false;
+            Console.WriteLine("This code will never run!");
+        }
+    }
+}
